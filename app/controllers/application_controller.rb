abebabd55fdcb654053bd2df4f9e36270e4f22c3
vanessasouza142 <vanessa.kbs16@gmail.com>
2023-05-07 12:39:14 -0300

    Add name and phone on devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,4 +15,16 @@ class ApplicationController < ActionController::Base
 
     include Devise::Controllers::Helpers
 
+
+
+    before_action :configure_permitted_parameters, if: :devise_controller?
+  
+    protected
+  
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up) do |u|
+        u.permit(:name, :phone, :email, :password, :password_confirmation)
+      end
+    end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -27,5 +27,29 @@ RSpec.describe User, type: :model do
       
       expect(user.valid?).to eq(false)
     end
+
+    it 'Validates if email field is present' do
+      user = build(:user)
+
+      expect(user.valid?).to eq(true)
+    end
+
+    it 'Validates if email field is not present' do
+      user = build(:user, email: nil)
+
+      expect(user.valid?).to eq(false)
+    end
+
+    it 'Validates if password field is present' do
+      user = build(:user)
+
+      expect(user.valid?).to eq(true)
+    end
+
+    it 'Validates if password field is not present' do
+      user = build(:user, password: nil)
+
+      expect(user.valid?).to eq(false)
+    end
   end
 end
