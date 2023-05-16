f5f9fc2c876ff53db484cea71bab1afdf88240ce
vanessasouza142 <vanessa.kbs16@gmail.com>
2023-05-15 22:55:34 -0300

    Redirect admin_root_path for user admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,20 @@
 class ApplicationController < ActionController::Base
-    layout :layout_by_resource
+    
+ # layout :layout_by_resource
 
-    private
+  #  private
 
-    def layout_by_resource
-        if devise_controller?
-            "devise"
-        else
-            "application"
-        end
-    end
+   # def layout_by_resource
+    #    if devise_controller?
+     #       "devise"
+      #  else
+       #     "application"
+       # end
+  #  end
 
     # before_action :authenticate_user!
 
-    include Devise::Controllers::Helpers
+ #   include Devise::Controllers::Helpers
 
 
 
@@ -24,6 +25,14 @@ class ApplicationController < ActionController::Base
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up) do |u|
         u.permit(:name, :phone, :email, :password, :password_confirmation)
+      end
+    end
+
+    def after_sign_in_path_for(resource)
+      if resource.is_a?(User) && resource.admin?
+        admin_root_path
+      else
+        root_path
       end
     end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,8 @@ class User < ApplicationRecord
 
   enum role: { user: 'user', admin: 'admin' }
 
+  def admin?
+    role == 'admin'
+  end
+
 end
